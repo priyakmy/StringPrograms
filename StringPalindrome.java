031cfd2506bf5cf00cc4390445a82204cf3814bd
PRIYA KUMARI <73944090+priyakmy@users.noreply.github.com>
2022-12-09 22:03:26 +0530

    Update StringPalindrome.java

--- StringPalindrome.java
+++ StringPalindrome.java
@@ -1,23 +1,24 @@
 import java.util.*;
-public class StringPalindrome {
-
- public static void main(String[] args) {
-		 String rev=" ";
-	 	  Scanner sc= new Scanner(System.in);  
-		System.out.print("Enter a string : ");  
-		String str= sc.nextLine();
-		String[] strArray = str.split(" ");
-		System.out.println("You have entered: "+str);
-		for(int i=str.length()-1;i>=0;i--){
-			rev=rev+ str.charAt(i);
-		}
-		if(str.equals(rev))
-		 {
-			System.out.println(str + "is a palindrome");
-		}
-			else{
-				System.out.println(str + "is  not a palindrome");
-			}
-	
-  }
+public class StringPalindrome
+{
+    public static void main(String args[])
+    {
+        String a, b = "";
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter the string you want to check:");
+        a = s.nextLine();
+        int n = a.length();
+        for(int i = n - 1; i >= 0; i--)
+        {
+            b = b + a.charAt(i);
+        }
+        if(a.equalsIgnoreCase(b))
+        {
+            System.out.println("The string is palindrome.");
+        }
+        else
+        {
+            System.out.println("The string is not palindrome.");
+        }
+    }
 }
